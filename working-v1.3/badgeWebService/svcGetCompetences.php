095ba6d42c2d6211302a9d03056611d446964d98
The User <user@email.com>
2025-12-10 07:31:06 -0300

    Lucas : clean SULYVAN's getCompetence.php

--- working-v1.3/badgeWebService/svcGetCompetences.php
+++ working-v1.3/badgeWebService/svcGetCompetences.php
@@ -1,11 +1,7 @@
 <?php
+
   include_once('./utils.php');
   include_once('./dataStorage.php');
-  
-  // DB open
-  include_once("./cfgDbEscape.php");
-  $db = new mysqli(DBESCAPE_HOST, DBESCAPE_LOGIN, DBESCAPE_PWD, DBESCAPE_NAME);
-  $db->set_charset("utf8");
 
   // Allow JSON content
   header("Content-Type: application/json; charset=UTF-8");
@@ -24,14 +20,13 @@
   foreach ($idCompetences as $idCompetence) {
     
     // filtered + escaped data
-    if (preg_match("/^[0-9]+$/", $idCompetence)) $idCompetence = $db->real_escape_string($idCompetence);
+    if (preg_match("/^[0-9]+$/", $idCompetence)) $idCompetence = $db->escape_string($idCompetence);
     // Get competence data
     $competence = DataStorage::getCompetence($idCompetence);
     // Add competence data to array
     if (!empty($competence)) $competences[] = $competence;
   }
-  // DB close
-  $db->close();
+
   if (empty($competences)) {
     echo json_encode(["success" => false, "message" => "Aucune donnée reçue"]);
     exit;
@@ -39,5 +34,5 @@
   // Send back a JSON response
   echo json_encode(['success'=>true,'competences'=>$competences]);
   exit(); 
-  ?>
 
+?>
